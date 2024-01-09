eddfffd68008f0af13e66030eac9641e162ce91e
Archit Sehgal <architsehgal19@gmail.com>
2024-01-09 12:32:47 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import React from "react";
-import { Text, View, ScrollView,StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text, View, ScrollView, StyleSheet } from "react-native";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import LittleLemonContent from "./components/LittleLemonContent";
 import LittleLemonWelcome from "./components/LittleLemonWelcome";
 import LittleLemonFeedback from "./components/LittleLemonFeedback";
 import LittleLemonLogin from "./components/LittlelemonLogin";
+
+const Stack=createNativeStackNavigator();
+
 export default function WelcomeApp() {
   return (
-    <View style={styles.container}>
-      <LittleLemonHeader />
-      <LittleLemonLogin/>
-      {/* <LittleLemonWelcome/> */}
-      {/* <LittleLemonFeedback/> */}
-      {/* <LittleLemonContent/> */}
-      <LittleLemonFooter />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <Stack.Navigator>
+          <Stack.Screen name="login" component={LittleLemonLogin}/>
+          {/* <Stack.Screen name="Welcome" component={LittleLemonWelcome}/> */}
+        </Stack.Navigator>
+        <LittleLemonFooter />
+      </View>
+    </NavigationContainer>
   )
 }
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#FFE666"
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFE666"
   }
 })
